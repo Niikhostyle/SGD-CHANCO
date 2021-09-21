e4a16418ec0c262c2a89b9d881f468d61162d4ea
marceloceballos <marcelo.ceballos@tide.cl>
2021-09-21 17:29:26 -0300

    nuevo usuario

---
+++ src/fe/app/Http/Requests/StoreUsuario.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Malahierba\ChileRut\ChileRut;
+use Malahierba\ChileRut\Rules\ValidChileanRut;
+
+class StoreUsuario extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+        'run'=>['required','max:10',new ValidChileanRut(new ChileRut)],
+        'id_perfil'=>'required',
+        'id_estado_usuario'=>'required',
+        'nombres'=>'required|max:20',
+        'primer_apellido'=>'required|max:20',
+        'segundo_apellido'=>'required|max:20',
+        'email'=>'required|unique:users|email',
+        ];
+    }
+
+    public function attributes()
+    {
+        return [
+            'id_perfil'=>'Perfil',
+            'id_estado_usuario'=>'Estado',
+            'run'=>'RUN',
+            'nombres'=>'Nombres',
+        ];
+    }
+
+    public function messages(){
+        return [
+            'run.ValidChileanRut'=>'RUN no es valido',
+            'run.ChileRut'=>'RUN no es valido'
+        ];
+    }
+}
