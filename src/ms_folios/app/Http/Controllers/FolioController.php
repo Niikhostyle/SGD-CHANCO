c7598de2194d1deea11bbd115834bd986d9b5278
alex <alex.toro@tide.cl>
2022-03-04 09:15:14 -0300

    cambios en folio

--- src/ms_folios/app/Http/Controllers/FolioController.php
+++ src/ms_folios/app/Http/Controllers/FolioController.php
@@ -28,21 +28,39 @@ class FolioController extends Controller{
         $nFolio = 1;
         $ultimoFolio = 0;
         $td = $request['id_tipo_documento'];
-        $prueba = false;
-        //return $prueba;
-        //tipo_folio, buzon, periodo, id_td, si no viene el buzon,  dependiendo del tipo de folio el buzon no va, hay 2 tipos de folio
         $datosRequest = $request->json()->all();
-        //return $td;
-        //folio tipo 1 asociado a tipo documento y año(perdiodo)
+      
         $datosFolio = TipoDocumentoBuzonFolio::all();
 
-        //$ultimoFolio = DB::table('tipo_documento_buzon_folio')->select('valor');
-        //$ultimoFolio  = DB::table('tipo_documento_buzon_folio')->max('valor') ;
-        //return $ultimoFolio;
+        
+       
+        foreach($datosFolio as $datos)
+        {
+            if( $datos['anio'] == $datosRequest['anio'])
+            {
+                if( $datos['id_tipo_documento'] == $datosRequest['id_tipo_documento'])
+                {
+
+                    $nFolio = $datos['valor'] +1;
+
+                }
+            }
+
+        }
+        // por tipo de documento y año
+        $documentoBuzon = TipoDocumentoBuzonFolio::updateOrCreate([
+            'id_tipo_documento' => $datosRequest['id_tipo_documento'],
+            'anio' => $datosRequest['anio'], 
+            
+        ],[
+            'id_tipo_documento' => $datosRequest['id_tipo_documento'],
+            'anio' => $datosRequest['anio'], 
+            'valor' => $nFolio,
+        ]);
+        
         
 
-
-        if ($datosRequest['id_buzon'] != null)
+        if ($datosRequest['id_tipo_folio'] == 2)
         {
 
             foreach($datosFolio as $datos)
@@ -51,112 +69,30 @@ class FolioController extends Controller{
                 {
                     if( $datos['id_tipo_documento'] == $datosRequest['id_tipo_documento'])
                     {
-                        if( $datos['id_buzon'] == $datosRequest['id_buzon'])
-                        {
-                            $nFolio = $datos['valor'] +1;
-                        }
                         
-
-                    }
-                }
-
-            }
-            //return $nFolio;
-            
-            $documentoBuzon = TipoDocumentoBuzonFolio::updateOrCreate([
-                'id_tipo_documento' => $datosRequest['id_tipo_documento'],
-                'id_buzon' => $datosRequest['id_buzon'],
-                'anio' => $datosRequest['anio'], 
-                
-            ],[
-                'id_tipo_documento' => $datosRequest['id_tipo_documento'],
-                'anio' => $datosRequest['anio'], 
-                'id_buzon' => $datosRequest['id_buzon'],
-                'valor' => $nFolio,
-            ]);
-        } else {
-            //$ultimoFolio = TipoDocumentoBuzonFolio::all('valor')->order_by('id_tipo_documento_buzon', 'desc')->first();
-            //$nFolio = $ultimoFolio+1;
-
-            foreach($datosFolio as $datos)
-            {
-                if( $datos['anio'] == $datosRequest['anio'])
-                {
-                    if( $datos['id_tipo_documento'] == $datosRequest['id_tipo_documento'])
-                    {
-
                         $nFolio = $datos['valor'] +1;
-
+            
                     }
                 }
-                
-               
-               
-                
+
             }
+            // por tipo de documento,buzon y año
             $documentoBuzon = TipoDocumentoBuzonFolio::updateOrCreate([
                 'id_tipo_documento' => $datosRequest['id_tipo_documento'],
+                'id_buzon' => $datosRequest['id_buzon'],
                 'anio' => $datosRequest['anio'], 
                 
             ],[
                 'id_tipo_documento' => $datosRequest['id_tipo_documento'],
                 'anio' => $datosRequest['anio'], 
+                'id_buzon' => $datosRequest['id_buzon'],
                 'valor' => $nFolio,
             ]);
-            
-
-            
-        }
+        } 
+        
 
         
         return $nFolio;
-            
-        
-        
+
     }
-
-    public function obtenerFolio(Request $request){
-        if($request->isJson())
-        {
-            try
-            {
-
-                $datosRequest = $request->json()->all();
-
-                $validator = $this->validator->validateFieldUser($datosRequest);
-
-                //if ($validator->fails())
-                  //  return $this->respondFail('Falla al listar los documentos: revisar datos de entrada');
-
-
-                return datatables(
-                    DB::table('documento_buzon')
-                    ->join('documento', 'documento_buzon.id_documento', '=', 'documento.id_documento')
-                    ->join('tipo_documento', 'documento.id_tipo_documento', '=', 'tipo_documento.id_tipo_documento')
-                    ->join('buzon', 'documento_buzon.id_buzon', '=', 'buzon.id_buzon')
-                    ->join('buzon_usuario', 'buzon.id_buzon', '=', 'buzon_usuario.id_buzon')
-                    ->select(
-                        'documento.folio as folio',
-                        'documento.fecha as fecha_documento',
-                        'tipo_documento.id_tipo_documento as id_tipo_documento',
-                        'buzon.id_buzon as id_buzon'
-                        )
-                    ->where('documento.id_documento','=',$datosRequest['id_documento'])
-                    //->where('buzon.id_buzon','=',1)
-                )
-                ->toJson();
-
-            }
-            catch (ModelNotFoundException $e)
-            {
-                return $this->respondError('No existen datos', 500);
-            }
-        }
-        else
-            return $this->respondError('Json inválido', 406);
-    }
-
-   
-
-
 }
