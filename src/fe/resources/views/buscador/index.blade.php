fa0db39b17af58a0e3c20326d40a3ad721a25be4
alex <alex.toro@tide.cl>
2022-04-06 11:16:14 -0400

    arreglo en el ver de buscador

--- src/fe/resources/views/buscador/index.blade.php
+++ src/fe/resources/views/buscador/index.blade.php
@@ -395,8 +395,7 @@
 <link rel="stylesheet" href="{{ asset('/vendor/tagsinput/bootstrap-tagsinput.css') }}">
 <link rel="stylesheet" href="{{ asset('/vendor/tagsinput/app.css') }}">
 <link rel="stylesheet" href="/css/bootstrap-multiselect.css" type="text/css"/>
-<link rel="stylesheet" href="https://cdn.datatables.net/buttons/1.6.2/css/buttons.dataTables.min.css">
-<link rel="stylesheet" href="https://cdn.datatables.net/1.10.23/css/dataTables.bootstrap5.min.css">
+
 
 
 
@@ -482,7 +481,7 @@
  
  
 
-<script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+
 <script src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.min.js"></script>
 <script src="https://cdn.datatables.net/buttons/2.2.2/js/dataTables.buttons.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
