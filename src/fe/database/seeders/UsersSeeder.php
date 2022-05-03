2d6a9643928c82d35224481d6856af64f0cedcad
Carolina Barrientos <carolina.barrientos@tide.cl>
2022-05-03 11:42:28 -0400

    Ajuste seeder users

--- src/fe/database/seeders/UsersSeeder.php
+++ src/fe/database/seeders/UsersSeeder.php
@@ -15,7 +15,7 @@ class UsersSeeder extends Seeder
     public function run()
     {
         $records = [
-            ['id_perfil' => '2', 'id_estado_usuario' => '1', 'run' => '11111111-1', 'nombre' => 'Administrador', 'email' => 'admin@mail.com', 'password' => '$2y$10$ATn2qykWPjNg2VhQ1q6SMuVT5HYNxZdcsbNGgBwc8o5ccSuF3PEkS','updated_at' => now(), 'created_at' => now()],           
+            ['id_perfil' => '2', 'id_estado_usuario' => '1', 'run' => '11111111-1', 'nombres' => 'Administrador', 'email' => 'admin@mail.com', 'password' => '$2y$10$ATn2qykWPjNg2VhQ1q6SMuVT5HYNxZdcsbNGgBwc8o5ccSuF3PEkS','updated_at' => now(), 'created_at' => now()],           
         ];
 
         DB::table('users')->insert($records);
