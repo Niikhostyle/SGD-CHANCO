2aac174597e70c1e518abd3e87d649912027005e
alex <alex.toro@tide.cl>
2022-05-04 09:47:15 -0400

    mail

--- src/fe/app/Mail/OrderShipped.php
+++ src/fe/app/Mail/OrderShipped.php
@@ -2,11 +2,13 @@
 
 namespace App\Mail;
 
+use App\Models\Buzon;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
 use App\Models\Documento;
+use App\Models\DocumentoBuzon;
 use App\Models\User;
 
 class OrderShipped extends Mailable
@@ -18,17 +20,24 @@ class OrderShipped extends Mailable
      * @var \App\Models\Documento
      * 
      */
-
-    public $user;   
+    public $subject = "";
+    public $user;
+    public $documento;   
+    public $documentoBuzon; 
+    public $buzon;
 
     /**
      * Create a new message instance.
      *
      * @return void
      */
-    public function __construct(User $user)
+    public function __construct(User $user, Documento $documento, DocumentoBuzon $documentoBuzon, Buzon $buzon)
     {
         $this->user = $user;
+        $this->documento = $documento;
+        $this->documentoBuzon = $documentoBuzon;
+        $this->buzon = $buzon;
+        $this->subject = "SGD - Nuevo documento ".' '." - ".' '. $documento->materia ;
     }
 
     /**
@@ -42,7 +51,7 @@ class OrderShipped extends Mailable
         return $this->markdown('mails.emails')
             ->with(['name' => $this->user->email]);
 
-        return $this->from('example@example.com')
-        ->markdown('mails.emails');
+        //return $this->from('example@example.com')
+        //->markdown('mails.emails');
     }
 }
