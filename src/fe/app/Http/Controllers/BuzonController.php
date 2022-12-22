da881230d81340c17a69cd44756e25f51d47ccd8
Paulo Lagos <paulo.lagos@tide.cl>
2022-12-22 17:20:27 -0300

    Ticket 6: primera versiion

--- src/fe/app/Http/Controllers/BuzonController.php
+++ src/fe/app/Http/Controllers/BuzonController.php
@@ -670,7 +670,9 @@ class BuzonController extends Controller
                         )
                     ->where('documento_buzon.id_buzon','=',$request->id_buzon)
                     ->where('documento_buzon.id_carpeta','=',$request->id_carpeta)
-                    ->whereYear('documento.created_at', $year_actual);
+                    ->whereYear('documento.created_at', $year_actual)
+                    //->whereRaw('documento_buzon.fecha = (select max(db.fecha) from documento_buzon db where db.id_documento = documento_buzon.id_documento and db.id_buzon = documento_buzon.id_buzon)')
+                    ;
                     
                     if($request->id_carpeta==3){
                         $datos->whereIn('documento_buzon.id_estado_documento',array(1,2)); //3- Despachado
