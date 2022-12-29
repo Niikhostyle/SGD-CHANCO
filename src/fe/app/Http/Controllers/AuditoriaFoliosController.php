4f461e3293d366e645f4022429a948f2a1b22898
Paulo Lagos <paulo.lagos@tide.cl>
2022-12-29 09:11:20 -0300

    Ticket 5: Incorporacion de listbox con buzones en filtro de busqueda, cambio buzon origen por buzon firma, ocultar seccion editable en creacion/edicion de documentos

--- src/fe/app/Http/Controllers/AuditoriaFoliosController.php
+++ src/fe/app/Http/Controllers/AuditoriaFoliosController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Buzon;
 use App\Models\TipoDocumento;
 use App\Models\TipoFolio;
 //use App\Providers\AppServiceProvider;
@@ -15,7 +16,9 @@ class AuditoriaFoliosController extends Controller
 
         $datosTipoFolio = TipoFolio::orderBy('nombre', 'ASC')->get();
 
-        return view('auditoria_folios.index',['tipos_folio'=>$datosTipoFolio]);
+        $datosBuzones = Buzon::orderBy('nombre', 'ASC')->get();
+
+        return view('auditoria_folios.index',['tipos_folio'=>$datosTipoFolio,'buzones'=>$datosBuzones]);
     }
 
     public function obtener_tipos_documentos(Request $request){
@@ -27,23 +30,27 @@ class AuditoriaFoliosController extends Controller
         return $tipos_documentos;
     }
 
+
     public function obtener_folios(Request $request){
         $tipo_folio = $request->tipo_folio;
         $tipo_documento = $request->tipo_documento;
+        $buzon = $request->buzon;
         DB::enableQueryLog(); // Enable query log
 
 
         $year_actual = session('year');   
-        //->whereYear('documento.created_at', $year_actual);
+
         $tWhere = " and 1 = 1 ";
         if($tipo_folio != ""){
-            //$tWhere .= " and lower(tf.nombre) = lower('".$tipo_folio."')";
             $tWhere .= " and td.id_tipo_folio = ".$tipo_folio."";
         }
         if($tipo_documento != ""){
-            //$tWhere .= " and lower(td.nombre) = lower('".$tipo_documento."')";
             $tWhere .= " and d.id_tipo_documento = ".$tipo_documento."";
         }
+        if($buzon != ""){
+            $tWhere .= " and b2.id_buzon = ".$buzon."";
+        }
+
         if($tWhere == " and 1 = 1 "){
             $tWhere = " and 1 = 2 ";
             $datos = DB::select("select 
@@ -54,39 +61,6 @@ class AuditoriaFoliosController extends Controller
                         d.folio is not null ".$tWhere." ");
         }
         else{            
-            // $datos = DB::select("select 
-            //             d.id_documento,to_char(d.fecha,'dd-mm-yyyy HH24:MI:SS') fecha_folio, d.folio ,b.nombre as buzon, d.materia,
-            //             (select bo.nombre from documento_buzon db2 join buzon bo on db2.id_buzon = bo.id_buzon  where db2.id_documento_buzon = db.id_documento_buzon_padre limit 1) as buzon_origen
-            //         from
-            //             documento as d
-            //             join tipo_documento as td on td.id_tipo_documento = d.id_tipo_documento
-            //             join documento_buzon as db on d.id_documento = db.id_documento
-            //             join buzon as b on b.id_buzon = db.id_buzon
-            //         where
-            //             d.folio is not null
-            //             and db.id_documento_buzon = (select max(db2.id_documento_buzon) from documento_buzon as db2 where db2.id_documento= d.id_documento)
-            //             and db.id_tipo_destino = 1
-            //             and EXTRACT(YEAR FROM d.created_at) = ".$year_actual." ".$tWhere." ");
-
-            // $datos = DB::select("select 
-            //                     d.folio ,
-            //                     to_char(d.fecha,'dd-mm-yyyy HH24:MI:SS') fecha_folio,  
-            //                     d.id_documento, b.nombre as buzon , 
-            //                     d.materia, 
-            //                     tf.nombre,td.nombre as nombre_documento   ,
-            //                     (select bo.nombre from documento_buzon db2 join buzon bo on db2.id_buzon = bo.id_buzon  where db2.id_documento_buzon = db.id_documento_buzon_padre limit 1) as buzon_origen
-            //                 from documento d
-            //                     join tipo_documento td on d.id_tipo_documento = td.id_tipo_documento 
-            //                     join tipo_folio tf on tf.id_tipo_folio = td.id_tipo_folio 
-            //                     join documento_buzon db on db.id_documento = d.id_documento 
-            //                     join buzon b on db.id_buzon = b.id_buzon 
-            //                 where
-            //                     d.folio is not null
-            //                     and db.id_tipo_destino = 1
-            //                     and EXTRACT(YEAR FROM d.created_at) = ".$year_actual." ".$tWhere."  
-            //                 group by tf.nombre,td.nombre , b.nombre , d.folio ,d.materia , to_char(d.fecha,'dd-mm-yyyy HH24:MI:SS'), d.id_documento,
-            //                 (select bo.nombre from documento_buzon db2 join buzon bo on db2.id_buzon = bo.id_buzon  where db2.id_documento_buzon = db.id_documento_buzon_padre limit 1)  ");
-            
             $datos = DB::select("select
                         d.folio ,
                         to_char(d.fecha,'dd-mm-yyyy HH24:MI:SS') fecha_folio,  
@@ -104,7 +78,7 @@ class AuditoriaFoliosController extends Controller
                         and db.id_documento_buzon = (select max(db2.id_documento_buzon) from documento_buzon db2 where db.id_documento = db2.id_documento  )
                         join documento_buzon db2 on db2.id_documento = d.id_documento
                         join buzon b2 on b2.id_buzon = db2.id_buzon
-                        and db2.id_documento_buzon_padre is null
+                        join documento_buzon_bitacora dbb on dbb.id_documento_buzon = db2.id_documento_buzon  and dbb.id_accion = 8
                     where 
                         d.folio is not null
                         and db.id_tipo_destino = 1
