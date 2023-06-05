f56d24b2cdd264d73812b89ec437d7f30c727a3c
Paulo Lagos <paulo.lagos@tide.cl>
2023-06-05 10:45:31 -0400

    Ajustes datos firma

--- src/ms_firma/app/Http/Controllers/FirmaController.php
+++ src/ms_firma/app/Http/Controllers/FirmaController.php
@@ -135,8 +135,8 @@ class FirmaController extends Controller
                     'api'       => 'https://api.firma.digital.gob.cl/firma/v2/files/tickets',
                     'purpose'   => 'Desatendido',
                     'entity'    => 'Municipalidad de Purranque',
-                    'tokenKey'  => 'e81b667a-0254-41d1-814a-cd247229f7ab',
-                    'secretKey' => 'be3cae200718419eb1d90725396bd6b8'
+                    'tokenKey'  => 'e81b667a-0254-41d1-8f4a-cd247229f7ab',
+                    'secretKey' => 'be3cae2007184f9eb1d90725396bd6b8'
                 );  
 
                 $classFirma = new FirmaBase($firmaDigitalConfig);
